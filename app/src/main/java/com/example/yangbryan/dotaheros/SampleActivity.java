2a034063f6f9e83709b65657b35eaa03dc1595b8
BryanYang <249763186@qq.com>
2015-10-10 10:59:34 +0800

    增加图片数据库

--- app/src/main/java/com/example/yangbryan/dotaheros/SampleActivity.java
+++ app/src/main/java/com/example/yangbryan/dotaheros/SampleActivity.java
@@ -7,20 +7,17 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import android.app.Activity;
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.widget.ArrayAdapter;
+
 import android.widget.ListView;
 import android.view.View;
 import android.widget.SimpleAdapter;
 
 import com.example.yangbryan.common.DataManager;
-import com.example.yangbryan.dotaheros.R;
-import com.example.yangbryan.dotaheros.ViewPagerIndicatorView;
+
 
 /**
  * 使用示例
@@ -30,7 +27,9 @@ import com.example.yangbryan.dotaheros.ViewPagerIndicatorView;
  */
 public class SampleActivity extends Activity {
 	private ViewPagerIndicatorView viewPagerIndicatorView;
-	private ListView listView;
+	private ListView listView1;
+	private ListView listView2;
+	private ListView listView3;
 	private DataManager dm;
 
 	@Override
@@ -43,32 +42,42 @@ public class SampleActivity extends Activity {
 		//set ViewPagerIndicatorView
 		this.viewPagerIndicatorView = (ViewPagerIndicatorView) findViewById(R.id.viewpager_indicator_view);
 		final TreeMap<String, View> map = new TreeMap<String, View>();
-		listView = new ListView(this);
-		SimpleAdapter sm =  new SimpleAdapter(this,getData(), R.layout.activity_sample_pager_1,new String[]{"img","name","dingwei"},new int[]{R.id.img,R.id.name,R.id.dingwei});
-		listView.setAdapter(sm);
-		map.put("力量", LayoutInflater.from(this).inflate(R.layout.activity_sample_pager_0, null));
-		map.put("敏捷", listView);
-		map.put("智力", LayoutInflater.from(this).inflate(R.layout.activity_sample_pager_2, null));
+		listView1 = new ListView(this);
+		SimpleAdapter sm1 =  new SimpleAdapter(this,getData("力量"), R.layout.activity_sample_pager_1,new String[]{"img","name_cn","position"},new int[]{R.id.img,R.id.name_cn,R.id.position});
+		listView1.setAdapter(sm1);
+		listView2 = new ListView(this);
+		SimpleAdapter sm2 =  new SimpleAdapter(this,getData("敏捷"), R.layout.activity_sample_pager_1,new String[]{"img","name_cn","position"},new int[]{R.id.img,R.id.name_cn,R.id.position});
+		listView2.setAdapter(sm2);
+		listView3 = new ListView(this);
+		listView1.setDivider(new ColorDrawable(getResources().getColor(android.R.color.holo_green_dark)));
+		listView1.setDividerHeight(2);
+		listView2.setDivider(new ColorDrawable(getResources().getColor(android.R.color.holo_green_dark)));
+		listView2.setDividerHeight(2);
+		listView3.setDivider(new ColorDrawable(getResources().getColor(android.R.color.holo_green_dark)));
+		listView3.setDividerHeight(2);
+		SimpleAdapter sm3 =  new SimpleAdapter(this,getData("智力"), R.layout.activity_sample_pager_1,new String[]{"img","name_cn","position"},new int[]{R.id.img,R.id.name_cn,R.id.position});
+		listView3.setAdapter(sm3);
+		map.put("力量", listView1);
+		map.put("敏捷", listView2);
+		map.put("智力", listView3);
 		this.viewPagerIndicatorView.setupLayout(map);
 	}
 
-	private List<Map<String, Object>> getData() {
+	private List<Map<String, Object>> getData(String type) {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		Map<String, Object> map = new HashMap<String, Object>();
-		SQLiteDatabase db = dm.openDatabase("yang");
-		Cursor c = db.rawQuery("SELECT * FROM heros", new String[]{});
+		SQLiteDatabase db = dm.openDatabase("dota");
+		Cursor c = db.rawQuery("SELECT * FROM heros where type = '" + type + "'", new String[]{});
 		while (c.moveToNext()) {
-			int _id = c.getInt(c.getColumnIndex("id"));
-			String name = c.getString(c.getColumnIndex("name"));
-			String dingwei = c.getString(c.getColumnIndex("dingwei"));
-			String info = c.getString(c.getColumnIndex("info"));
-			String img = c.getString(c.getColumnIndex("img"));
+			String name_cn = c.getString(c.getColumnIndex("name_cn"));
+			String position = c.getString(c.getColumnIndex("position"));
+			String img = c.getString(c.getColumnIndex("name_en"));
 			int imgId = getResources().getIdentifier(img, "drawable", getPackageName());
 
 			map = new HashMap<String, Object>();
-			map.put("img", R.drawable.earthshaker_vert);
-			map.put("name", name);
-			map.put("dingwei", dingwei);
+			map.put("img", imgId);
+			map.put("name_cn", name_cn);
+			map.put("position", position);
 			list.add(map);
 		}
 		c.close();
